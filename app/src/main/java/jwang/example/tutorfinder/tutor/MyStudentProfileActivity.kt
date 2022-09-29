bf9d9b6809def834592b06c54172e920bf9194d8
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-09-28 20:30:43 -0400

    Editting student's course and grade

--- app/src/main/java/jwang/example/tutorfinder/tutor/MyStudentProfileActivity.kt
+++ app/src/main/java/jwang/example/tutorfinder/tutor/MyStudentProfileActivity.kt
@@ -1,7 +1,12 @@
 package jwang.example.tutorfinder.tutor
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import jwang.example.tutorfinder.R
 
@@ -9,6 +14,9 @@ import jwang.example.tutorfinder.R
 class MyStudentProfileActivity : AppCompatActivity() {
 
     var studentId = 0
+    //lateinit var editTextDialog: EditText
+    lateinit var tvGrade: TextView
+    lateinit var tvCourse: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,10 @@ class MyStudentProfileActivity : AppCompatActivity() {
         studentId = intent.getIntExtra(TutorScreenActivity.STUDENT_ID, 0)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Student Profile"
+        tvCourse = findViewById(R.id.textViewCourse)
+        tvGrade = findViewById(R.id.textViewGrade)
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -28,4 +40,48 @@ class MyStudentProfileActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    fun onButtonClick(view: View) {
+        when(view.id){
+            R.id.imageButtonGrade -> setDialog("Grade")
+
+            R.id.imageButtonCourse -> setDialog("Course")
+
+        }
+
+
+    }
+
+    private fun setDialog(title:String) {
+        val editTextDialog = EditText(this)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setView(editTextDialog)
+        builder.setCancelable(false)
+        builder.setPositiveButton("SAVE"){
+            dialog, which ->
+            run {
+                Log.d("Student", "save: $title")
+                if (title == "Grade") {
+                    tvGrade.text = editTextDialog.text.toString()
+                }
+                if (title == "Course") {
+                    tvCourse.text = editTextDialog.text.toString()
+                }
+            }
+        }
+        builder.setNegativeButton("CANCEL"){
+                dialog, which ->
+            run {
+                Log.d("Student",which.toString())
+                dialog.cancel()
+            }
+
+        }
+        builder.show()
+
+    }
+
+
+
 }
