f811a9566712c559181504500f73b82308f03236
pradip khatri <p_khatri168959@fanshaweonline.ca>
2022-10-01 13:52:57 -0400

    testing for remote connection

---
+++ app/src/main/java/jwang/example/tutorfinder/NavigateActivity.kt
@@ -0,0 +1,11 @@
+package jwang.example.tutorfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigate)
+    }
+}
