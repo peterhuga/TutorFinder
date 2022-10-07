7706162f512692e2f437a85cbbf49c9ac0a95aee
slsamban <s_wanniadipaththumud@fanshaweonline.ca>
2022-10-07 16:59:46 -0400

    Added dummy recycler view items

--- app/src/main/java/jwang/example/tutorfinder/tutor/StudentRequestActivity.kt
+++ app/src/main/java/jwang/example/tutorfinder/tutor/StudentRequestActivity.kt
@@ -2,11 +2,31 @@ package jwang.example.tutorfinder.tutor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import jwang.example.tutorfinder.R
 
 class StudentRequestActivity : AppCompatActivity() {
+
+    //Dummy data for populating UI
+    companion object {
+        val students: ArrayList<Student> = arrayListOf(
+            Student(101, "Sampath", 21, "sampath@email.com"),
+            Student(102, "Jianwei", 22, "jianwei@email.com"),
+            Student(103,"Sunny", 19, "Sunny@email.com")
+        )
+
+
+        const val STUDENT_ID = "student id"
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_request)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewStudentRequests)
+        val adapter = MyStudentsRvAdapter(TutorScreenActivity.students)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/jwang/example/tutorfinder/tutor/StudentRequestsRvAdapter.kt
@@ -0,0 +1,69 @@
+package jwang.example.tutorfinder.tutor
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import jwang.example.tutorfinder.R
+import jwang.example.tutorfinder.tutor.TutorScreenActivity.Companion.STUDENT_ID
+
+
+class StudentRequestsRvAdapter(private val dataset: ArrayList<Student>): RecyclerView.Adapter<StudentRequestsRvAdapter.ViewHolder>() {
+
+    val TAG = "Tutor"
+
+
+
+    //var onItemClick: ((Student)->Unit)? = null
+
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+            val nameTextView = itemView.findViewById<TextView>(R.id.textViewMyStudentItemName)
+            val ageTextView = itemView.findViewById<TextView>(R.id.textViewMyStudentItemAge)
+            val emailTextView = itemView.findViewById<TextView>(R.id.textViewMyStudentItemEmail)
+            val imageView = itemView.findViewById<ImageView>(R.id.imageViewMyStudentItem)
+            val acceptButton = itemView.findViewById<ImageView>(R.id.imageViewAccept)
+            val rejectButton = itemView.findViewById<ImageView>(R.id.imageViewReject)
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.student_requests_rv_item, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.nameTextView.text = "Name: ${item.name}"
+        holder.ageTextView.text = "Age: ${item.age}"
+        holder.emailTextView.text = "Email: ${item.email}"
+        //Hardcoded for now. Include image resource id property in data model later.
+        holder.imageView.setImageResource(R.drawable.ic_launcher_foreground)
+
+//        onItemClick?.invoke(item)
+
+
+        holder.itemView.setOnClickListener{
+            Log.d(TAG,"item clicked")
+
+                val studentID = item.id
+                val intent = Intent(it.context, MyStudentProfileActivity::class.java)
+                intent.putExtra(STUDENT_ID, studentID)
+                it.context.startActivity(intent)
+
+        }
+
+    }
+}
