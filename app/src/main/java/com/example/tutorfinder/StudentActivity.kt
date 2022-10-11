3765d0133ece12113dcfdfb3ecbc9f23da6a0a10
sunny tamang <lamasunny55@gmail.com>
2022-10-11 01:19:48 -0400

    createdfilterTutor
    created filter tutor ui

--- app/src/main/java/com/example/tutorfinder/StudentActivity.kt
+++ app/src/main/java/com/example/tutorfinder/StudentActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tutorfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class StudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+    }
+
+    fun onFilterTutorBtnPressed(view: View) {
+        startActivity(Intent(this,FilterTutorActivity::class.java))
+    }
+    fun onEditProfileclicked(view: View) {
+        startActivity((Intent(this,EditStudentProfileActivity::class.java)))
     }
 }
---
+++ app/src/main/java/com/example/tutorfinder/Tutor.kt
@@ -0,0 +1,4 @@
+package com.example.tutorfinder
+
+data class Tutor (val name:String, val age:String, val gender: String,val address:String, val Phone:String, val grade:String, val course:String,val email: String){
+}
