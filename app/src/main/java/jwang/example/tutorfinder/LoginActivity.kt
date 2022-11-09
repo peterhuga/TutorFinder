7becd641769e887fbd5dbed47c29014c77e7c625
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-11-08 21:24:38 -0500

    Before jianwei3 pull

--- app/src/main/java/jwang/example/tutorfinder/LoginActivity.kt
+++ app/src/main/java/jwang/example/tutorfinder/LoginActivity.kt
@@ -105,7 +105,7 @@ class LoginActivity : AppCompatActivity() {
 //        startActivity(Intent(this, SignUpActivity::class.java))
 //    }
 
-    fun onToTutorClicked(view: View) {
-        startActivity(Intent(this,TutorScreenActivity::class.java))
-    }
+//    fun onToTutorClicked(view: View) {
+//        startActivity(Intent(this,TutorScreenActivity::class.java))
+//    }
 }
