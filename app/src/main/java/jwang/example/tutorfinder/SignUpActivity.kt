5410a0fc5b3f8491cccb057fa3061d0e5d9358ca
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-11-12 01:36:32 -0500

    Role based auth and returning user auto login done.

--- app/src/main/java/jwang/example/tutorfinder/SignUpActivity.kt
+++ app/src/main/java/jwang/example/tutorfinder/SignUpActivity.kt
@@ -2,91 +2,121 @@ package jwang.example.tutorfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
     lateinit var emailEditText: EditText
     lateinit var passwordEditText : EditText
-    private lateinit var auth: FirebaseAuth
-    lateinit var MytutorBtn:Button
-    lateinit var MyStudentBtn:Button
+    lateinit var checkStudent: CheckBox
+    lateinit var checkTutor: CheckBox
+
+    lateinit var signUpButton: Button
+    private lateinit var database: DatabaseReference
+    var role = "student"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        MytutorBtn=findViewById(R.id.TutorBtn)
-        MyStudentBtn=findViewById(R.id.studentBtn)
-        MytutorBtn.isSelected=false
-        MyStudentBtn.isSelected=false
-        auth = Firebase.auth
 
+
+        signUpButton = findViewById(R.id.newUserSignupBtn)
         emailEditText=findViewById(R.id.signinusernameedittext)
         passwordEditText=findViewById(R.id.signinpasswordedittext)
-    }
+        checkStudent = findViewById(R.id.checkBoxStudent)
+        checkTutor = findViewById(R.id.checkBoxTutor)
+        database = Firebase.database.reference
 
-    fun signupNewUserBtn(view: View) {
+        checkTutor.setOnCheckedChangeListener{
+                _, isChecked -> checkStudent.isEnabled = !isChecked
+        }
+
+        checkStudent.setOnCheckedChangeListener{
+                _, isChecked -> checkTutor.isEnabled = !isChecked
+        }
 
 
-        val email = emailEditText.text.toString()
-        val password = passwordEditText.text.toString()
-        if(email.isNotBlank() && password.isNotBlank()) {
-            when (view.id) {
-                R.id.studentBtn -> {
+        signUpButton.setOnClickListener {
 
-                    auth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("signup", "createUserWithEmail:success")
-                                finish()
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("signupfail", "createUserWithEmail:failure", task.exception)
-                                Toast.makeText(
-                                    baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
+
+
+            //Log.d("mytag", "key, $key")
+            when {
+                TextUtils.isEmpty(emailEditText.text.toString().trim {it <= ' '}) -> {
+                    Toast.makeText(this,"Please enter email", Toast.LENGTH_SHORT).show()
                 }
-                R.id.TutorBtn -> {
-                    auth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("signup", "createUserWithEmail:success")
-                                finish()
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("signupfail", "createUserWithEmail:failure", task.exception)
-                                Toast.makeText(
-                                    baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
+                TextUtils.isEmpty(passwordEditText.text.toString().trim {it <= ' '}) -> {
+                    Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show()
                 }
+                else -> {
+                    //Decide role
+                    when(true){
+                        checkStudent.isChecked -> role = "student"
+                        checkTutor.isChecked -> role = "tutor"
+                        else -> Toast.makeText(this, "Please select a role", Toast.LENGTH_LONG).show()
+                    }
+                    val user:String = emailEditText.text.toString().trim {it <= ' '}
+                    val pass:String = passwordEditText.text.toString().trim {it <= ' '}
+                    Log.d("mytag", user + pass)
+                    // signs in current user
+//                    FirebaseAuth.getInstance().signInWithEmailAndPassword(user,pass)
+                    // create a new user and password for registration
+                    Firebase.auth.createUserWithEmailAndPassword(user,pass)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> {
+                                    task ->
+                                Log.d("mytag", task.isSuccessful.toString())
+                                if(task.isSuccessful) {
+                                    
+                                    //fetch needed info from the newly created user
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    //val firebaseUser: FirebaseUser = Firebase.auth.currentUser!!
+                                    val uId = firebaseUser.uid
+                                    val email = firebaseUser.email
+                                    Toast.makeText(this,R.string.welcome_user, Toast.LENGTH_SHORT).show()
 
+                                    //Based on the role variable value, user's node is created in student or tutor node
+                                    val ref = database.child("users")
 
+                                    //TODO: why does when statement not work?
+//                                    var newUser = when(role){
+//                                                    "student" -> {Student(email!!, role)
+//                                                    Log.d("myTag", "user")}
+//                                                    "tutor" -> Tutor(email!!, role)
+//                                                    else -> Student(email!!, role)
+//                                                }
+                                    var newUser = if (role == "student")
+                                        Student(email!!, role)
+                                        else
+                                        Tutor(email!!, role)
+                                    ref.child(uId).setValue(newUser)
+                                    //finish()
+                                    onBackPressed()
+                                }
+                                else {
+                                    Toast.makeText(this,R.string.problem, Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        )
+                }
             }
         }
-
-
-
-
-        else {
-            Toast.makeText(baseContext, "Email and password cannot be blank",
-                Toast.LENGTH_SHORT).show()
-        }
-
     }
+
+
 
 }
